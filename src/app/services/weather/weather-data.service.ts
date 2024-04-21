2f980cb863d1f1fd7379918e313d707cd5dad814
Samuel <samuelhauptmannvandam@gmail.com>
2024-04-21 20:57:48 +0200

    Added interface

--- src/app/services/weather/weather-data.service.ts
+++ src/app/services/weather/weather-data.service.ts
@@ -1,3 +1,45 @@
+export interface WeatherData {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -9,7 +51,7 @@ export class WeatherDataService {
 
   constructor(private http: HttpClient) { }
 
-  fetchWeatherData(city: string): Observable<any> {
-    return this.http.get<any>(`https://localhost:7241/WeatherForecast?city=${city}`);
+  fetchWeatherData(city: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(`https://localhost:7241/WeatherForecast?city=${city}`);
   }
 }
